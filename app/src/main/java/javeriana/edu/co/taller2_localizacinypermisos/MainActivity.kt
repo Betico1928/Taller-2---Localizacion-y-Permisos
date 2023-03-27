a61a58efa873488c5d2037d5b4946aa9195bdb67
Betico1928 <betoveluis@gmail.com>
2023-03-26 23:09:21 -0500

    MainActivity onClickListeners implemented.

--- app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/MainActivity.kt
+++ app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/MainActivity.kt
@@ -1,7 +1,9 @@
 package javeriana.edu.co.taller2_localizacinypermisos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import javeriana.edu.co.taller2_localizacinypermisos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
@@ -12,5 +14,45 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         bindingMain = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingMain.root)
+
+        initializeElements()
+
+        Log.i("Taller 2", "MainActivity created")
+    }
+
+    private fun initializeElements()
+    {
+        // Camera button:
+        bindingMain.imageButtonCamera.setOnClickListener{
+
+            Log.i("Taller 2", "Camera button pressed.")
+        }
+
+
+        // Contacts button:
+        bindingMain.imageButtonContacts.setOnClickListener{
+
+            Log.i("Taller 2", "Contacts button pressed.")
+
+            //val goToCameraActivity = Intent
+        }
+
+
+        // OSM button:
+        bindingMain.imageButtonOSM.setOnClickListener {
+
+            Log.i("Taller 2", "OSM button pressed.")
+
+            //val goToOSMActivity = Intent
+        }
+
+
+        // Google Maps button:
+        bindingMain.imageButtonOSM.setOnClickListener {
+
+            Log.i("Taller 2", "Google Maps button pressed.")
+
+            //val goToGoogleMapsActivity = Intent
+        }
     }
 }
