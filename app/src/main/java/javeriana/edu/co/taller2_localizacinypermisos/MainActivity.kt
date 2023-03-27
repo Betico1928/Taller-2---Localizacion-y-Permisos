cfba5d443c70d9bc4160b182054d0a6dff611e67
Betico1928 <betoveluis@gmail.com>
2023-03-26 22:59:11 -0500

    MainActivity bindings initialized

--- app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/MainActivity.kt
+++ app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/MainActivity.kt
@@ -2,10 +2,15 @@ package javeriana.edu.co.taller2_localizacinypermisos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javeriana.edu.co.taller2_localizacinypermisos.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var bindingMain : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
     }
 }
