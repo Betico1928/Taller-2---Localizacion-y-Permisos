161ba0926ca25d8b9e9cf2bf973c29bdcbfcf81e
Betico1928 <betoveluis@gmail.com>
2023-03-27 00:36:31 -0500

    Created OSM Activity.

---
+++ app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/OSMActivity.kt
@@ -0,0 +1,11 @@
+package javeriana.edu.co.taller2_localizacinypermisos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OSMActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_osmactivity)
+    }
+}
