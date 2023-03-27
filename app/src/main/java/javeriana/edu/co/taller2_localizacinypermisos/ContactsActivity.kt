e9e889788940b00a6f8d559ec59cd466e743ea99
Betico1928 <betoveluis@gmail.com>
2023-03-27 14:31:22 -0500

    PermissionRequest implemented.

--- app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/ContactsActivity.kt
+++ app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/ContactsActivity.kt
@@ -1,13 +1,43 @@
 package javeriana.edu.co.taller2_localizacinypermisos
 
+import android.content.pm.PackageManager
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import javeriana.edu.co.taller2_localizacinypermisos.databinding.ActivityContactsBinding
 
 class ContactsActivity : AppCompatActivity()
 {
     private lateinit var bindingContacts : ActivityContactsBinding
+
+    // Solicitud de permiso
+    private var contactsPermission = registerForActivityResult(
+
+        // Request permission
+        ActivityResultContracts.RequestPermission(),
+        ActivityResultCallback{contactPermissionStatus ->
+
+            Log.i("Taller 2", "Contacts Permission: $contactPermissionStatus")
+
+            if (contactPermissionStatus)
+            {
+                bindingContacts.statusContacts.text = "Permission Granted"
+                bindingContacts.statusContacts.setTextColor(Color.GREEN)
+            }
+            else
+            {
+                bindingContacts.statusContacts.text = "Permission Denied"
+                bindingContacts.statusContacts.setTextColor(Color.RED)
+            }
+        }
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -15,12 +45,11 @@ class ContactsActivity : AppCompatActivity()
         bindingContacts = ActivityContactsBinding.inflate(layoutInflater)
         setContentView(bindingContacts.root)
 
-        initializeElements()
+        getContactsPermission()
     }
 
-    private fun initializeElements()
+    private fun getContactsPermission()
     {
-        bindingContacts.statusContacts.text = "NO STATUS"
-        bindingContacts.statusContacts.setTextColor(Color.YELLOW)
+        contactsPermission.launch(android.Manifest.permission.READ_CONTACTS)
     }
 }
