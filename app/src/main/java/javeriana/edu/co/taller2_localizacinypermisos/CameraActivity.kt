852fb99be09722cdbe730094a9a20c919c205702
Betico1928 <betoveluis@gmail.com>
2023-04-03 18:22:45 -0500

    Gallery functionality implemented

--- app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/CameraActivity.kt
+++ app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/CameraActivity.kt
@@ -1,7 +1,11 @@
 package javeriana.edu.co.taller2_localizacinypermisos
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import javeriana.edu.co.taller2_localizacinypermisos.databinding.ActivityCameraBinding
 
 class CameraActivity : AppCompatActivity()
@@ -9,10 +13,32 @@ class CameraActivity : AppCompatActivity()
     // Binding
     private lateinit var bindingCamera: ActivityCameraBinding
 
+    // Request Gallery
+    val galleryRequest = registerForActivityResult(ActivityResultContracts.GetContent(), ActivityResultCallback { imagePath : Uri? -> loadImage(imagePath) })
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         bindingCamera = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(bindingCamera.root)
+
+        initializeElements()
+    }
+
+    private fun initializeElements()
+    {
+        bindingCamera.galleryButton.setOnClickListener {
+            // Open gallery
+            galleryRequest.launch("image/*")
+        }
+    }
+
+    // Load the image in the activity
+    private fun loadImage( imagePath : Uri? )
+    {
+        val imageStream = contentResolver.openInputStream( imagePath!! )
+        val image = BitmapFactory.decodeStream( imageStream )
+
+        bindingCamera.cameraImageView.setImageBitmap( image )
     }
 }
