20dd5061b9222904406a6f654bb3e0b621466d1e
Betico1928 <betoveluis@gmail.com>
2023-04-03 22:59:31 -0500

    Live location working

--- app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/GoogleMapsActivity.kt
+++ app/src/main/java/javeriana/edu/co/taller2_localizacinypermisos/GoogleMapsActivity.kt
@@ -1,12 +1,21 @@
 package javeriana.edu.co.taller2_localizacinypermisos
 
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,6 +23,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 import javeriana.edu.co.taller2_localizacinypermisos.databinding.ActivityGoogleMapsBinding
 
 class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -21,22 +31,10 @@ class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityGoogleMapsBinding
 
-    /*
-    //location permission
-    val locationPermission = registerForActivityResult(
-        ActivityResultContracts.RequestPermission(),
-        ActivityResultCallback {
-            if(it) {
-                Log.i("Taller 2", "Hay permiso: " + it.toString())
-                locationSettings()
-            }
-            else{
-                Log.i("Taller 2", "Permiso denegado")
-                binding.elevationA.text = "Permiso denegado"
-            }
-        }
-    )
-    */
+    //location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +46,55 @@ class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getCurrentLocation()
     }
+
+    private fun getCurrentLocation()
+    {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf( android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+        }
+        else
+        {
+            Toast.makeText(this, "No se pudo obtener la ubicación", Toast.LENGTH_SHORT).show()
+            /*
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? -> location?.let {
+
+                val currentLatLng = LatLng(it.latitude, it.longitude)
+                mMap.addMarker(MarkerOptions().position(currentLatLng).title("Tu ubicación"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+            } ?: run {
+                Toast.makeText(this, "No se pudo obtener la ubicación", Toast.LENGTH_SHORT).show()
+            }
+            }
+
+             */
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
@@ -62,10 +108,13 @@ class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        /*
         mMap.animateCamera(
             CameraUpdateFactory.newLatLngZoom(sydney, 10f),
             4000,
             null
         )
+
+         */
     }
 }
